1552e2f1faa8eed2c076ce0bd04d0585d1fc417d
unknown <youssouf.tiefa@gmail.com>
2021-12-07 15:49:26 +0100

    add social adapter

--- src/test/java/com/adservio/authentification/auth/service/SocialServiceTest.java
+++ src/test/java/com/adservio/authentification/auth/service/SocialServiceTest.java
@@ -2,6 +2,7 @@ package com.adservio.authentification.auth.service;
 
 import com.adservio.authentification.auth.IntegrationTest;
 import com.adservio.authentification.auth.service.dto.SocialUserConnectionDto;
+import com.adservio.authentification.auth.service.mapper.SocialUserConnectionMapper;
 
 @IntegrationTest
 public class SocialServiceTest {
