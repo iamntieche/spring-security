b3c3187a707a2a4d3e4f9d834089e695cd3a450a
unknown <youssouf.tiefa@gmail.com>
2022-01-05 15:44:37 +0100

    BadRequestAlertException : update parent class

--- src/main/java/com/mfoumgroup/authentification/auth/exception/BadRequestAlertException.java
+++ src/main/java/com/mfoumgroup/authentification/auth/exception/BadRequestAlertException.java
@@ -3,45 +3,24 @@ package com.mfoumgroup.authentification.auth.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestAlertException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
-
-    private final String entityName;
-
-    private final String errorKey;
-    private final URI type;
-
-    private final String defaultMessage;
 
     public BadRequestAlertException(String defaultMessage, String entityName, String errorKey) {
         this(ErrorConstants.DEFAULT_TYPE, defaultMessage, entityName, errorKey);
     }
 
     public BadRequestAlertException(URI type, String defaultMessage, String entityName, String errorKey) {
-        this.entityName = entityName;
-        this.errorKey = errorKey;
-        this.type = type;
-        this.defaultMessage = defaultMessage;
     }
 
-    public String getEntityName() {
-        return entityName;
-    }
 
-    public String getErrorKey() {
-        return errorKey;
-    }
 
-    private static Map<String, Object> getAlertParameters(String entityName, String errorKey) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("message", "error." + errorKey);
-        parameters.put("params", entityName);
-        return parameters;
-    }
+
+
 
 }
