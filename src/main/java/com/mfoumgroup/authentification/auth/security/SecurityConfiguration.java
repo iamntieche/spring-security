f9067c98bc69fb4f1eb99d70dc614e51e5737814
unknown <youssouf.tiefa@gmail.com>
2022-01-06 14:17:04 +0100

    update

--- src/main/java/com/mfoumgroup/authentification/auth/security/SecurityConfiguration.java
+++ src/main/java/com/mfoumgroup/authentification/auth/security/SecurityConfiguration.java
@@ -63,8 +63,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-       // http.csrf().disable();
+
         http.headers().contentSecurityPolicy(CONTENTSECURITYPOLICY)
                 .and()
                 .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
